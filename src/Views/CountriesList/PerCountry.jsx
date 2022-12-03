5d8cb62c6dea6350e08687e96fc98d5b19d020e4
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 11:38:16 +0300

    Split Country Stats

---
+++ src/Views/CountriesList/PerCountry.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Typography from "../../Components/Base/Typography";
+import Table from "../../Components/Table";
+
+export default function PerCountryStats({ setCountry }) {
+  const [searchValue, setSearchValue] = useState("");
+
+  const countriesTestData = [
+    {
+      "Country": "Jordan",
+      "CountryCode": "JO",
+      "Slug": "jordan",
+      "NewConfirmed": 13,
+      "TotalConfirmed": 323,
+      "NewDeaths": 0,
+      "TotalDeaths": 5,
+      "NewRecovered": 16,
+      "TotalRecovered": 74,
+      "Date": "2020-04-05T06:37:00Z"
+    },
+    {
+      "Country": "Kenya",
+      "CountryCode": "KE",
+      "Slug": "kenya",
+      "NewConfirmed": 4,
+      "TotalConfirmed": 126,
+      "NewDeaths": 0,
+      "TotalDeaths": 4,
+      "NewRecovered": 0,
+      "TotalRecovered": 4,
+      "Date": "2020-04-05T06:37:00Z"
+    },
+  ];
+
+  const [filteredTableData, setFilteredTableData] = useState(countriesTestData);
+
+  const tableColumns = [
+    { key: "Country" },
+    { key: "NewConfirmed" },
+    { key: "TotalConfirmed" },
+    { key: "NewDeaths" },
+    { key: "TotalDeaths" },
+    { key: "NewRecovered" },
+    { key: "TotalRecovered" },
+    { title: "Last Updated", key: "Date" },
+  ];
+
+  const onSearchChange = e => {
+    const { value } = e.target;
+
+    setSearchValue(value);
+
+    value.length ? (
+      setFilteredTableData(countriesTestData.filter(record => record.Country.toUpperCase().includes(value.toUpperCase())))
+    ) : (
+      setFilteredTableData(countriesTestData)
+    );
+  };
+
+  const onCountryView = record => {
+    setCountry(record);
+  };
+
+  return (
+    <div className="py-4">
+      <Typography size={6}>
+        Per Country
+      </Typography>
+
+      <Form.Label>
+        Search by country
+      </Form.Label>
+
+      <Form.Control
+        name="search"
+        type="text"
+        value={searchValue}
+        onChange={onSearchChange}
+      />
+
+      <Table
+        columns={tableColumns}
+        data={filteredTableData}
+        onView={onCountryView}
+      />
+    </div>
+  )
+};
