a745bc155bd88ac0b99864f4e6561e5b97b19642
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 14:02:09 +0300

    <a> Tag Fix

--- src/Layout/Footer/index.jsx
+++ src/Layout/Footer/index.jsx
@@ -1,5 +1,4 @@
 import Navbar from 'react-bootstrap/Navbar';
-
 import Container from '../../Components/Base/Grid/Container';
 
 export default function Footer() {
@@ -7,9 +6,9 @@ export default function Footer() {
     <Navbar bg="light" fixed="bottom">
       <Container className="mx-5 my-3 justify-content-center">
         All Rights Reserved For
-        <a href="https://suhaib.dev/" target="_blank" className="mx-1">Suhaib Ahmad</a>,
-        <a href="https://nadsoft.net/" target="_blank" className="mx-1">NadSoft</a>&
-        <a href="https://covid19api.com/" target="_blank" className="mx-1">COVID19 API</a>
+        <a href="https://suhaib.dev/" target="_blank" rel="noreferrer" className="mx-1">Suhaib Ahmad</a>,
+        <a href="https://nadsoft.net/" target="_blank" rel="noreferrer" className="mx-1">NadSoft</a>&
+        <a href="https://covid19api.com/" target="_blank" rel="noreferrer" className="mx-1">COVID19 API</a>
         | {new Date().getFullYear()}
       </Container>
     </Navbar>
