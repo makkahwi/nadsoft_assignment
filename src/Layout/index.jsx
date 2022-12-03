81c8dc33177e78eec5895556593472463bb43d7a
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 09:10:40 +0300

    Build & Engage Initial Header & Footer Codes

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,9 +1,14 @@
 import React from "react"
 
+import Footer from "./Footer"
+import Header from "./Header"
+
 export default function Layout() {
   return (
     <>
+      <Header />
       Layout
+      <Footer />
     </>
   )
 };
