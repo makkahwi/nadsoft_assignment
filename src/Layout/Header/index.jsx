eee5c7444fa30f7054391488fdbdf237ce9428fe
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 10:08:07 +0300

    Layout Base Components Build & Engage

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -1,8 +1,8 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, useLocation } from 'react-router-dom';
 
+import Container from '../../Components/Base/Container';
+import Nav from '../../Components/Base/Nav';
 import routes from "../../Routes"
 
 export default function Header() {
