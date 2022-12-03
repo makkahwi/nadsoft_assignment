0471a2bfadbac5d1ca32bcbadc9bbf049d86d81c
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 11:06:50 +0300

    Add Table Columns Title Generator

--- src/Components/Table/index.jsx
+++ src/Components/Table/index.jsx
@@ -2,13 +2,15 @@ import { render } from '@testing-library/react';
 import Table from '../Base/Table';
 
 export default function TableComp({ columns, data, className, ...props }) {
+  const generateColumnTitle = key => key?.split("")?.map((letter, i) => i === 0 ? letter : letter === letter.toUpperCase() ? ` ${letter}` : letter).join("")
+
   return (
     <Table striped bordered hover responsive className={`my-3 text-center ${className}`} {...props}>
       <thead>
         <tr>
           {columns?.map((column, i) => (
             <th key={i}>
-              {column.title || column.key}
+              {column.title || generateColumnTitle(column.key)}
             </th>
           ))}
         </tr>
