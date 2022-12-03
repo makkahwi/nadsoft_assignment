9098d5d4cd05c4568cb97efe3d20d8de0318a58d
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 09:30:54 +0300

    Build Initial Header UI

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -1,9 +1,24 @@
-import React from "react"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Header() {
   return (
-    <>
-      Header
-    </>
-  )
-};
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          Covid Statistics
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
