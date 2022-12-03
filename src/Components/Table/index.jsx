17dafe9e9ccbee232ffa7ad9f62a5893b44be531
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 11:06:26 +0300

    Update Table To Handle No Data Case

--- src/Components/Table/index.jsx
+++ src/Components/Table/index.jsx
@@ -3,7 +3,7 @@ import Table from '../Base/Table';
 
 export default function TableComp({ columns, data, className, ...props }) {
   return (
-    <Table striped bordered hover responsive className={`my-3 ${className}`} {...props}>
+    <Table striped bordered hover responsive className={`my-3 text-center ${className}`} {...props}>
       <thead>
         <tr>
           {columns?.map((column, i) => (
@@ -15,15 +15,23 @@ export default function TableComp({ columns, data, className, ...props }) {
       </thead>
 
       <tbody>
-        {data?.map((record, i) => (
-          <tr key={i}>
-            {columns?.map((column, y) => (
-              <td key={y}>
-                {column.render ? render(record) : record[column.key]}
-              </td>
-            ))}
+        {data.length ? (
+          data?.map((record, i) => (
+            <tr key={i}>
+              {columns?.map((column, y) => (
+                <td key={y}>
+                  {column.render ? render(record) : record[column.key]}
+                </td>
+              ))}
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan="100%">
+              No Results Found
+            </td>
           </tr>
-        ))}
+        )}
       </tbody>
     </Table>
   );
