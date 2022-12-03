770cf727fe7164216641a0cf06eeafea58fc7a18
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 09:19:33 +0300

    Build & Engage Routes In Layout

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { Suspense } from "react"
+import { Navigate, Route, Routes } from 'react-router-dom'
 
+import routes from "../Routes"
 import Footer from "./Footer"
 import Header from "./Header"
 
@@ -7,7 +9,23 @@ export default function Layout() {
   return (
     <>
       <Header />
-      Layout
+      <Suspense fallback={"Loading"}>
+        <Routes>
+          {routes?.map((route, i) => {
+            return (
+              <Route
+                key={i}
+                path={route.path}
+                exact={route.exact}
+                name={route.name}
+                element={<route.element />}
+              />
+            )
+          })}
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   )
