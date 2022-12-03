27ddab6b8dc2f8b2c1e9dc180356f130bd638919
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 13:27:16 +0300

    Setup MockData Trigger

--- src/API/index.js
+++ src/API/index.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+import { countriesTestData, globalTestData } from "../Helpers/mockData"
 
 export const baseURL = process.env.REACT_APP_BACKEND_URL || "https://api.covid19api.com";
+
+export const mockStatus = false;
+
+const mockData = { Global: globalTestData, Countries: countriesTestData };
 
 const service = axios.create({
   baseURL,
@@ -23,17 +28,25 @@ const service = axios.create({
 
 service.interceptors.response.use(
   response => {
-    return response.data;
+    if (mockStatus) {
+      return mockData
+    } else {
+      return response.data;
+    }
   },
   error => {
-    const e = String(error);
+    if (mockStatus) {
+      return mockData
+    } else {
+      const e = String(error);
 
-    if (e.includes("401") || e.includes("403")) {
-      console.log("Error", e);
-      // store.dispatch(logout());
-      // window.location.href = ("/")
-      // store.dispatch(addNotification({ error: true, action: "Executed", message: i18n.t("This is Unauthorized Access") }));
-      // return Promise.reject("Your session has expired. Please sign in again to continue.");
+      if (e.includes("401") || e.includes("403")) {
+        console.log("Error", e);
+        // store.dispatch(logout());
+        // window.location.href = ("/")
+        // store.dispatch(addNotification({ error: true, action: "Executed", message: i18n.t("This is Unauthorized Access") }));
+        // return Promise.reject("Your session has expired. Please sign in again to continue.");
+      }
     }
   }
 );
--- src/Helpers/mockData.js
+++ src/Helpers/mockData.js
@@ -1,10 +1,10 @@
 export const globalTestData = {
-  "NewConfirmed": 100282,
-  "TotalConfirmed": 1162857,
-  "NewDeaths": 5658,
-  "TotalDeaths": 63263,
-  "NewRecovered": 15405,
-  "TotalRecovered": 230845
+  "NewConfirmed": 1082,
+  "TotalConfirmed": 1157,
+  "NewDeaths": 58,
+  "TotalDeaths": 633,
+  "NewRecovered": 155,
+  "TotalRecovered": 2845
 };
 
 export const countriesTestData = [
