6078c59199ab807039cf82b4e69108f78b8ec52d
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 12:33:02 +0300

    Split Countries MockData

--- src/Helpers/mockData.js
+++ src/Helpers/mockData.js
@@ -6,3 +6,30 @@ export const globalTestData = {
   "NewRecovered": 15405,
   "TotalRecovered": 230845
 };
+
+export const countriesTestData = [
+  {
+    "Country": "Jordan",
+    "CountryCode": "JO",
+    "Slug": "jordan",
+    "NewConfirmed": 13,
+    "TotalConfirmed": 323,
+    "NewDeaths": 0,
+    "TotalDeaths": 5,
+    "NewRecovered": 16,
+    "TotalRecovered": 74,
+    "Date": "2020-04-05T06:37:00Z"
+  },
+  {
+    "Country": "Kenya",
+    "CountryCode": "KE",
+    "Slug": "kenya",
+    "NewConfirmed": 4,
+    "TotalConfirmed": 126,
+    "NewDeaths": 0,
+    "TotalDeaths": 4,
+    "NewRecovered": 0,
+    "TotalRecovered": 4,
+    "Date": "2020-04-05T06:37:00Z"
+  },
+];
--- src/Views/StatsPage/PerCountry.jsx
+++ src/Views/StatsPage/PerCountry.jsx
@@ -20,33 +20,6 @@ export default function PerCountryStats({ setCountry }) {
     getData();
   }, [])
 
-  const countriesTestData = [
-    {
-      "Country": "Jordan",
-      "CountryCode": "JO",
-      "Slug": "jordan",
-      "NewConfirmed": 13,
-      "TotalConfirmed": 323,
-      "NewDeaths": 0,
-      "TotalDeaths": 5,
-      "NewRecovered": 16,
-      "TotalRecovered": 74,
-      "Date": "2020-04-05T06:37:00Z"
-    },
-    {
-      "Country": "Kenya",
-      "CountryCode": "KE",
-      "Slug": "kenya",
-      "NewConfirmed": 4,
-      "TotalConfirmed": 126,
-      "NewDeaths": 0,
-      "TotalDeaths": 4,
-      "NewRecovered": 0,
-      "TotalRecovered": 4,
-      "Date": "2020-04-05T06:37:00Z"
-    },
-  ];
-
   const [filteredTableData, setFilteredTableData] = useState(countriesStats);
 
   const tableColumns = [
