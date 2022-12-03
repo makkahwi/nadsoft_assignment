43ec5aca87cee8e37f0180de37c6d06cbb6b77b4
makkahwi <suhaibahmadai@hotmail.com>
2022-12-03 10:48:21 +0300

    Update Routes' Labels

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -5,9 +5,9 @@ const Country = lazy(() => import('../Views/CountryDetails'))
 const About = lazy(() => import('../Views/About'))
 
 const routes = [
-  { path: '/', exact: true, name: 'Home Page', element: Landing },
+  { path: '/', exact: true, name: 'Home', element: Landing },
   { path: '/details', exact: true, name: 'Country Details', element: Country },
-  { path: '/about', exact: true, name: 'About Page', element: About },
+  { path: '/about', exact: true, name: 'About', element: About },
 ];
 
 export default routes
